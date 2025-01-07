e65261f396006ba2b0b022c75f03bbc4bcb0d50b
Robert <r.s.garofalo87@gmail.com>
2025-01-07 12:17:51 +1100

    initialize project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,30 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "15px"
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    fontFamily: {
+      primary: ['Poiret One', 'sans-serif'],
+      secondary: ['Montserray', 'sans-serif']
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#050206',
+        secondary: '#ffffff',
+        luxury: '#9A8174',
+        elegant: '#3B3B3B',
+        accent: {
+          DEFAULT: '#5597d4',
+          hover: '#327EC4',
+        },
       },
     },
   },
